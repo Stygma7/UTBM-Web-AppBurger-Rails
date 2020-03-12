8a8ad722384f20baa38ef2c64ca566d497a4a4fc
Matthieu-Hirth <matthieu.hirth.68@gmail.com>
2020-03-12 15:35:41 +0100

    Add table with nutriments from OpenFoodFacts

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,5 +1,15 @@
 class Burger < ApplicationRecord
     def nutriments
+        require 'openfoodfacts'
 
+        product = Openfoodfacts::Product.get(code, locale: 'fr')
+
+        if product == nil
+            product = [["rien"], ["toujours rien"]]
+        else
+            product = product.nutriments
+        end
+
+        return product
     end
 end
